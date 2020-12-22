9c1e6f2ff828f75c9133a8f6cf5e2b65b387d515
ShriyaBijam <shriya.bijam@gmail.com>
2020-12-22 15:08:05 +0530

    added images and content

--- news_letter/edition_1_2021/urls.py
+++ news_letter/edition_1_2021/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('edition_1_2021/', views.edition_1_2021_home, name='edition_1_2021_home')
+    path('edition_1_2021/', views.edition_1_2021_home, name='edition_1_2021_home'),
+    path('edition_1_2021/content', views.edition_1_2021_content, name='edition_1_2021_content'),
+    path('edition_1_2021/images', views.edition_1_2021_images, name='edition_1_2021_images')
 ]
--- news_letter/edition_1_2021/views.py
+++ news_letter/edition_1_2021/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def edition_1_2021_home(request):
     return render(request, 'edition_1_2021/home.html')
+
+def edition_1_2021_content(request):
+    return render(request, 'edition_1_2021/content.html')
+
+def edition_1_2021_images(request):
+    return render(request, 'edition_1_2021/images.html')
