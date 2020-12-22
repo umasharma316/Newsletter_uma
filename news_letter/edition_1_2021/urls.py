ee63c5f7f61b4e4ddb99deff3e2f9fbc8a3d266f
ShriyaBijam <shriya.bijam@gmail.com>
2020-12-22 16:40:26 +0530

    Added book review and non tech content and images

--- news_letter/edition_1_2021/urls.py
+++ news_letter/edition_1_2021/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('edition_1_2021/', views.edition_1_2021_home, name='edition_1_2021_home'),
-    path('edition_1_2021/content', views.edition_1_2021_content, name='edition_1_2021_content'),
+    path('edition_1_2021/content_tech', views.edition_1_2021_content_tech, name='edition_1_2021_content_tech'),
+    path('edition_1_2021/content_non_tech', views.edition_1_2021_content_non_tech, name='edition_1_2021_content_non_tech'),
+    path('edition_1_2021/content_book', views.edition_1_2021_content_book, name='edition_1_2021_content_book'),
     path('edition_1_2021/images', views.edition_1_2021_images, name='edition_1_2021_images')
 ]
--- news_letter/edition_1_2021/views.py
+++ news_letter/edition_1_2021/views.py
@@ -5,8 +5,14 @@ from django.http import HttpResponse
 def edition_1_2021_home(request):
     return render(request, 'edition_1_2021/home.html')
 
-def edition_1_2021_content(request):
-    return render(request, 'edition_1_2021/content.html')
+def edition_1_2021_content_tech(request):
+    return render(request, 'edition_1_2021/content_tech.html')
+
+def edition_1_2021_content_non_tech(request):
+    return render(request, 'edition_1_2021/content_non_tech.html')
+
+def edition_1_2021_content_book(request):
+    return render(request, 'edition_1_2021/content_book.html')
 
 def edition_1_2021_images(request):
     return render(request, 'edition_1_2021/images.html')
