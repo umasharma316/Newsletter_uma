fe4ade3f1403ca12eab66535a98a9533fce6c5df
ShriyaBijam <shriya.bijam@gmail.com>
2020-12-28 14:46:22 +0530

    Added Nav bar

--- news_letter/static/js/script.js
+++ news_letter/static/js/script.js
@@ -1,13 +1,36 @@
-window.onload = function() {
-    // Get the Object by Tag
-    var allObjects = document.getElementsByTagName("object");
-    // Get the SVG document inside the Object tag
-    var i;
-    for (i = 0; i < allObjects.length; i++) {
-        var objDoc = allObjects[i].contentDocument;
-        var svg = objDoc.getElementsByTagName("svg")[0];
-        var bbox = svg.getBBox();
-        var viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ");
-        svg.setAttribute("viewBox", viewBox);
+$('a[href^="#"]').click(function(event) {
+    var id = $(this).attr("href");
+    var target = $(id).offset().top;
+    $('html, body').animate({scrollTop:target}, 500);
+    event.preventDefault();
+});
+
+function getTargetTop(elem){
+var id = elem.attr("href");
+var offset = 60;
+return $(id).offset().top - offset;
+}
+
+
+$(window).scroll(function(e){
+    isSelected($(window).scrollTop())
+});
+
+var sections = $('a[href^="#"]');
+
+function isSelected(scrolledTo){
+
+var threshold = 100;
+var i;
+
+for (i = 0; i < sections.length; i++) {
+    var section = $(sections[i]);
+    var target = getTargetTop(section);
+   
+    if (scrolledTo > target - threshold && scrolledTo < target + threshold) {
+        sections.removeClass("active");
+        section.addClass("active");
     }
+
 };
+}
